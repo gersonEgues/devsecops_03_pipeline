0e30d8c4dd57f8c5334bdd6d1c1469f31f337130
gersoncl <gerson.choque.loreno@gmail.com>
2025-07-18 18:40:53 -0400

    update test file

--- test/sample.test.js
+++ test/sample.test.js
@@ -3,3 +3,4 @@ console.log("Test ejecutado correctamente 2");
 console.log("Test ejecutado correctamente 3");
 console.log("Test ejecutado correctamente 4");
 console.log("Test ejecutado correctamente 5");
+console.log("Test ejecutado correctamente 6");
